f7c1c5401f2c6a5ab1b96af1dccdd4680b8f9f1b
Kyntal1 <clandres1985@gmail.com>
2023-09-20 14:23:08 -0300

    [NUEVO] Primer commit del proyecto.

---
+++ desafioaccenture/src/main/java/com/desafio/desafioaccenture/DesafioaccentureApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.desafioaccenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioaccentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioaccentureApplication.class, args);
+	}
+
+}
---
+++ desafioaccenture/src/test/java/com/desafio/desafioaccenture/DesafioaccentureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.desafioaccenture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioaccentureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
